6790d24a870d093abc8522a1bd1857f16cdb24a4
nikola <nikola.zeka97@gmail.com>
2022-01-17 21:03:14 +0100

    ingretiends api completed

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,10 +1,11 @@
 from django.urls.conf import include, path
-from recipe.views import TagViewSet
+from recipe.views import TagViewSet, IngredientViewSet
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register('tags', TagViewSet )
+router.register('tags', TagViewSet)
+router.register('ingredients', IngredientViewSet)
 
 app_name = 'recipe'
 
